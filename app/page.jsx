a80b9cf86f41128bfc4fd7faddbbd7e1b5d3f58f
Uday Mungalpara <105286180+techUdayMungalpara@users.noreply.github.com>
2023-06-01 10:44:51 +0530

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -1,4 +1,18 @@
 import Link from "next/link";
+
+export const metadata = {
+  title: "CHECK TITLE TAG ONLY CHECK CHECK ",
+  description:
+    "The growing convergence of the designer and programmer roles by way of React",
+  openGraph: {
+    title: "Pure UI",
+    description:
+      "The growing convergence of the designer and programmer roles by way of React",
+    images: [{ url: "/next.svg" }],
+  },
+};
+
+
 export default function Home() {
   return (
     <>
